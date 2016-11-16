c4aed7d980ec4af6b47f021bb551f2896898609c
biofractal <biofractal@gmail.com>
2016-11-16 12:11:45 +0000

    backup and restore of groups

--- src/service/groups/totals.js
+++ src/service/groups/totals.js
@@ -1,4 +1,36 @@
+const groupTransactions = (groups, transactions) => {
+  const grouped = [...transactions]
+  grouped.forEach(t => {
+    t.groups = []
+    const dl = t.description.toLowerCase()
+    groups.forEach(g => {
+      if (g.matches.includes(match => dl.indexOf(match.toLowerCase()) !== -1)) {
+        t.groups.push(g.id)
+      }
+    })
+  })
+  grouped.forEach(t => {
+    if (t.groups.length === 0) t.groups.push(0)
+  })
+
+  return grouped
+}
+
 const calculate = (groups, transactions) => {
+  const grouped = groupTransactions(groups, transactions)
+  const results = [...groups]
+  noMatch = {id: 0, label: 'Not Matched', matches: ['*'], amount: 0}
+  results.push(noMatch)
+  results.forEach(g => {
+    transactions.forEach(t => {
+      if (t.groups.includes(tg => g.id === tg.id)) g.amount += t.amount
+    })
+  })
+
+  return results
+}
+
+const calculatex = (groups, transactions) => {
   const result = []
   let noMatch = groups.find(g => g.id === 0)
   if (!noMatch) {
